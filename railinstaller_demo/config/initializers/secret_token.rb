bc156ef57c5351905c09f668e4304571fac2cc2d
Richard Kulp <rick@grassrootsunwired.com>
2013-01-31 11:22:28 -0500

    Deleted misnamed file.

--- railinstaller_demo/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-RailinstallerDemo::Application.config.secret_token = '4ada545e663899d46029df9d66a941f5c2b13d0117940d03ae9dabfa7bd403d7b20cf02a6f8b476e1b151c62f129da48f0047c49619e683217e38c1f4c6d5ff8'
